753934dd49a797cbdf7bcfa80b0e7c96e96b0d28
Tako <takogiorgobiani97@gmail.com>
2022-08-23 21:22:13 +0400

    FOR EACH method

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -112,7 +112,7 @@ console.log(letters.join(' - '));
  */
 
 /////////// THE AT METHOD
-
+/* 
 const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
@@ -124,3 +124,32 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+ */
+
+////////////// FOR EACH FUNCTION/////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: ${Math.abs(movement)}`);
+  }
+}
+
+console.log('-----FOR EACH ---------');
+
+movements.forEach(function (movement, i /* index */, arr) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: ${Math.abs(movement)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function (450)
+// 2: function (400)
+// ....
