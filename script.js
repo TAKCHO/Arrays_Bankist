80c8fa453b7c895882f06ba37b308f19064cc646
Tako <takogiorgobiani97@gmail.com>
2022-08-26 22:54:49 +0400

    the reduce method pt 2

--- script.js
+++ script.js
@@ -81,6 +81,14 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+console.log(calcDisplayBalance);
+
 const creatUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -269,6 +277,18 @@ console.log(movements);
 const balance = movements.reduce(function (acc, cur, i, arr) {
   console.log(`Iteration ${i}: ${acc}`);
   return acc + cur;
-}, 0);
+}, 100);
 
 console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+//// MAXIMUM VALUE
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max);
