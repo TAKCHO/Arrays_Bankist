90f1f1e204a96bfd4150cb847deac2f653915a15
Tako <takogiorgobiani97@gmail.com>
2022-08-28 22:06:53 +0400

    the flat method

--- script.js
+++ script.js
@@ -451,7 +451,7 @@ console.log(account);
 
  */
 
-/// SOME AND EVERY METHODS
+/* /// SOME AND EVERY METHODS
 
 console.log(movements);
 console.log(movements.includes(-130));
@@ -471,3 +471,19 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+ */
+
+///// FLAT AND FLATMAP METHODS
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2)); // 2 is the level
+
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+const overalBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
