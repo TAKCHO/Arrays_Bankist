143e7878ced04b6ded1e227ef511cb2c2940cd00
Tako <takogiorgobiani97@gmail.com>
2022-08-28 22:00:52 +0400

    the every method

--- script.js
+++ script.js
@@ -456,8 +456,18 @@ console.log(account);
 console.log(movements);
 console.log(movements.includes(-130));
 
-// CONDITION
+// SOME: CONDITION
 console.log(movements.some(mov => mov === -130));
 
 const anyDeposits = movements.some(mov => mov > 0);
 console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
