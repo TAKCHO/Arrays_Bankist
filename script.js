13c935032738c62e5b7744dd5398ef3b3d514fbc
Tako <takogiorgobiani97@gmail.com>
2022-08-31 22:12:42 +0400

    Array Method Parctice pt3

--- script.js
+++ script.js
@@ -621,3 +621,24 @@ const sums = accounts
   );
 
 console.log(sums);
+
+// N4
+// this is a nice title -> This Is a Nice Title
+
+const convertTitleCase = function (title) {
+  const capitilize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitilize(word)))
+    .join(' ');
+  return capitilize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title'));
+console.log(convertTitleCase('this is a LONG title but not too long '));
+console.log(convertTitleCase('and here is another title with an EXAMPLE '));
